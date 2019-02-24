abe1e6d9fe677a135c7ed50da12d20db3c2d8def
joann675 <joann675@yahoo.com>
2019-02-23 19:37:54 -0500

    Final version

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Spicy Black Bean Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Double Onion Bison Slider', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Chipotle Chili Turkey Burger', true);
+
+
